43babad1602e8ada69a35178024e35598fabf712
Ramz001 <rkenjaev1@gmail.com>
2025-03-14 06:22:24 +0500

    testing source control vs code

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, type ServiceAccount } from "firebase-admin/app";
 
 const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}",
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
 ) as ServiceAccount;
 
 if (!initializeApp.length) {
