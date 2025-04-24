bdece35f81da9da8a076dffd5e5533eb825772a2
Ramz001 <rkenjaev1@gmail.com>
2025-04-24 08:28:38 +0500

    implementing yandex metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { ThemeProvider } from "next-themes";
 import PWASetup from "@/components/PWASetup";
 import { LayoutWrapper } from "./layout-wrapper";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
+import YandexMetrika from "@/lib/analytics/YandexMetrika";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,6 +45,24 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className + " dark"} suppressHydrationWarning>
+        <Script id="metrika-counter" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(${process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID || ""}, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`}
+        </Script>
+        <Suspense fallback={<></>}>
+          <YandexMetrika />
+        </Suspense>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
---
+++ lib/analytics/YandexMetrika.tsx
@@ -0,0 +1,18 @@
+'use client'
+ 
+import { useEffect } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
+ 
+export default function YandexMetrika() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+ 
+  useEffect(() => {
+    const url = `${pathname}?${searchParams}`
+    //@ts-ignore
+    ym(process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID || "", 'hit', url);
+ 
+  }, [pathname, searchParams])
+ 
+  return null
+}
