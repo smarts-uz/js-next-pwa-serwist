eace62fbd137f92b5878129e9dc0c9464fe264a3
Ramz001 <rkenjaev1@gmail.com>
2025-04-26 23:26:52 +0500

    NavigationRoute implementation example

--- app/sw.ts
+++ app/sw.ts
@@ -1,6 +1,6 @@
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist, NetworkOnly, StaleWhileRevalidate } from "serwist";
+import { NavigationRoute, Serwist } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -12,32 +12,41 @@ declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
-  precacheOptions: {
-    cleanupOutdatedCaches: true,
-  },
-  skipWaiting: true,
-  clientsClaim: true,
-  navigationPreload: true,
-  runtimeCaching: [
-    ...defaultCache,
-    {
-      matcher: ({ request }) => request.destination === "style",
-      handler: new StaleWhileRevalidate(),
-    },
-  ],
-  offlineAnalyticsConfig: true,
-  disableDevLogs: true,
-  importScripts: ["/custom-sw.js"],
-  fallbacks: {
-    entries: [
-      {
-        url: "/offline",
-        matcher({ request }) {
-          return request.destination === "document";
-        },
-      },
-    ],
-  },
+  // precacheOptions: {
+  //   cleanupOutdatedCaches: true,
+  // },
+  // skipWaiting: true,
+  // clientsClaim: true,
+  // navigationPreload: true,
+  runtimeCaching: [...defaultCache],
+  // offlineAnalyticsConfig: true,
+  // disableDevLogs: true,
+  // importScripts: ["/custom-sw.js"],
+  // fallbacks: {
+  //   entries: [
+  //     {
+  //       url: "/offline",
+  //       matcher({ request }) {
+  //         return request.destination === "document";
+  //       },
+  //     },
+  //   ],
+  // },
 });
+
+// get all urls from serwist & log them
+const urls = serwist.getPrecachedUrls();
+console.log(urls);
+
+const result = serwist.registerRoute(
+  new NavigationRoute(
+    serwist.createHandlerBoundToUrl("http://localhost:3001/offline"),
+    {
+      allowlist: [],
+      denylist: [],
+    }
+  )
+);
+console.log(result);
 
 serwist.addEventListeners();
