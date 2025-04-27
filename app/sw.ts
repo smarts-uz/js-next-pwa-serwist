3778039d28fc7f7080932759adb1199a0b1ba80c
Ramz001 <rkenjaev1@gmail.com>
2025-04-28 02:13:12 +0500

    BackgroundSync Advanced usage through Custom Plugin

--- app/sw.ts
+++ app/sw.ts
@@ -1,5 +1,14 @@
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist, PrecacheFallbackPlugin, NetworkOnly } from "serwist";
+import type {
+  PrecacheEntry,
+  SerwistGlobalConfig,
+  SerwistPlugin,
+} from "serwist";
+import {
+  Serwist,
+  NetworkOnly,
+  BackgroundSyncPlugin,
+  BackgroundSyncQueue,
+} from "serwist";
 import { defaultCache } from "@serwist/next/worker";
 
 declare global {
@@ -14,10 +23,10 @@ const serwist = new Serwist({
   precacheOptions: {
     cleanupOutdatedCaches: true,
   },
+  runtimeCaching: [...defaultCache],
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-
   offlineAnalyticsConfig: true,
   // disableDevLogs: true,
   importScripts: ["/custom-sw.js"],
@@ -33,16 +42,50 @@ const serwist = new Serwist({
   // },
 });
 
+const queue = new BackgroundSyncQueue("myQueueName");
+
+const statusPlugin = {
+  fetchDidSucceed({ response }) {
+    if (response.status >= 500) {
+      // Throwing anything here will trigger fetchDidFail.
+      throw new Error("Server error.");
+    }
+    // If it's not 5xx, use the response as-is.
+    return response;
+  },
+} satisfies SerwistPlugin;
+
+// const backgroundSync = new BackgroundSyncPlugin("myQueueName", {
+//   maxRetentionTime: 24 * 60, // Retry for a maximum of 24 Hours (specified in minutes)
+// });
+
 serwist.registerCapture(
-  /^\/$/,
+  /\/api\/.*\/*.json/,
   new NetworkOnly({
-    plugins: [
-      new PrecacheFallbackPlugin({
-        fallbackUrls: ["/offline"],
-        serwist,
-      }),
-    ],
-  })
+    plugins: [statusPlugin],
+  }),
+  "POST"
 );
+
+self.addEventListener("fetch", (event) => {
+  // Add in your own criteria here to return early if this
+  // isn't a request that should use background sync.
+  if (event.request.method !== "POST") {
+    return;
+  }
+
+  const backgroundSync = async () => {
+    try {
+      const response = await fetch(event.request.clone());
+      console.log("response", response);
+      return response;
+    } catch (error) {
+      await queue.pushRequest({ request: event.request });
+      return Response.error();
+    }
+  };
+
+  event.respondWith(backgroundSync());
+});
 
 serwist.addEventListeners();
